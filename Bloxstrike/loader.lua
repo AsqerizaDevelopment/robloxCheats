2b69a3f2a0a043d6e9237556bc96d10cd1658b99
DeFlopper <asqerizadevelopment@gmail.com>
2026-03-01 16:07:40 +0100

    Fix baseURL for loading BloxstrikeESP script

--- Bloxstrike/loader.lua
+++ Bloxstrike/loader.lua
@@ -1,8 +1,8 @@
 local Bloxstrike = {}
-local baseURL = "https://raw.githubusercontent.com/AsqerizaDevelopment/robloxCheats/main/Bloxstrike/src/utils/"
+local baseURL = "https://raw.githubusercontent.com/AsqerizaDevelopment/robloxCheats/main/Bloxstrike/src/"
 
 Bloxstrike.utils = {}
-Bloxstrike.utils.ESP = loadstring(game:HttpGet(baseURL .. "BloxstrikeESP.lua"))()
+Bloxstrike.utils.ESP = loadstring(game:HttpGet(baseURL .. "utils/BloxstrikeESP.lua"))()
 Bloxstrike.UI = loadstring(game:HttpGet(baseURL .. "BloxstrikeUI.lua"))()
 
 return Bloxstrike
